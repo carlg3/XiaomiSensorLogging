f2034a553f169f90cb08f0d4537aa72a2fb21c0e
gamberoillecito <itjustfordrive@gmail.com>
2022-11-27 17:54:29 +0100

    ugly prototype works

--- provaDash.py
+++ provaDash.py
@@ -1,68 +1,159 @@
 from dash import Dash, dcc, Output, Input, html
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import sqlite3
 import pandas as pd
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+## Database management
 DBNAME = 't_h_readings.db'
-con  = sqlite3.connect(DBNAME,
-                      detect_types=sqlite3.PARSE_DECLTYPES |
-                        sqlite3.PARSE_COLNAMES)
 
-cur = con.cursor()
-query = '''SELECT
-                timestamp as "timestamp [timestamp]",
-                temperature,
-                avg(temperature) OVER
-                    (ORDER BY timestamp ROWS BETWEEN 10 PRECEDING AND 10 FOLLOWING)
-                    as wtemperature,
-                humidity,
-                avg(humidity) OVER
-                    (ORDER BY timestamp ROWS BETWEEN 10 PRECEDING AND 10 FOLLOWING)
-                    as whumidity
-            FROM reading ORDER BY timestamp;'''
-df = pd.read_sql_query(query, con)
+def get_data(sm_range):
+    con  = sqlite3.connect(DBNAME,
+                          detect_types=sqlite3.PARSE_DECLTYPES |
+                            sqlite3.PARSE_COLNAMES)
 
-mygraph = dcc.Graph(figure={})
+    cur = con.cursor()
 
-dropdown = dcc.Dropdown(options=['wtemperature', 'whumidity'],
-        value='wtemperature',
-        clearable=False)
-temp_button = dbc.Button("Temperature")
-humi_button = dbc.Button("Humidity")
+    query = '''SELECT
+                    timestamp as "timestamp [timestamp]",
+                    temperature,
+                    avg(temperature) OVER
+                        (ORDER BY timestamp ROWS BETWEEN ? PRECEDING AND ? FOLLOWING)
+                        as wtemperature,
+                    humidity,
+                    avg(humidity) OVER
+                        (ORDER BY timestamp ROWS BETWEEN ? PRECEDING AND ? FOLLOWING)
+                        as whumidity
+                FROM reading ORDER BY timestamp;'''
+    df = pd.read_sql_query(query, con, params=[sm_range, sm_range, sm_range, sm_range])
+    return df
 
-radio = dbc.RadioItems(
-            id="radios",
-            className="btn-group",
-            inputClassName="btn-check",
-            labelClassName="btn btn-outline-primary",
-            labelCheckedClassName="active",
-            options=[
-                {"label": "Temperature", "value": 'wtemperature'},
-                {"label": "Humidity", "value": 'whumidity'}
-            ],
-            value='wtemperature',
-        )
-button_group = html.Div(
-    [
-        radio,
-        html.Div(id='output'),
-    ],
-    className='radio-group',
+def get_statistics(l1=None, l2=None, fields=['temperature', 'humidity']):
+    con  = sqlite3.connect(DBNAME,
+                          detect_types=sqlite3.PARSE_DECLTYPES |
+                            sqlite3.PARSE_COLNAMES)
+
+    cur = con.cursor()
+
+    query = '''SELECT\n'''
+    stat = ['avg', 'max', 'min']
+    for f in fields:
+        for s in stat:
+            query += f'''round({s}({f}),1) as {s}_{f}'''
+            if stat.index(s) != len(stat) - 1:
+                query += ',\n'
+        if fields.index(f) != len(fields) - 1:
+            query += ',\n'
+    
+    query += '''\nFROM reading'''
+    params = []
+    if (l1 is not None and l2 is not None):
+        query += '\nWHERE (timestamp BETWEEN ? AND ?);' 
+        params = [l1, l2]
+    else:
+        query += ';'
+    df = pd.read_sql_query(query, con, params=params)
+    #print(query)
+    return df
+
+#################################
+# Create figure with secondary y-axis
+
+mygraph1 = dcc.Graph(
+        figure={},
+        config={
+            "displaylogo": False,
+            'modeBarButtonsToRemove': ['pan2d', 'zoomin', 'zoomout']
+        })
+
+fig1 = make_subplots(specs=[[{"secondary_y": True}]])
+# Add traces
+df = get_data(10)
+fig1.add_trace(
+        go.Scatter(x=df.timestamp, y=df.wtemperature, name="Temperature", line=dict(color='firebrick')),
+    #go.Scatter(x=[1, 2, 3], y=[40, 50, 60], name="yaxis data"),
+    secondary_y=False,
 )
 
-app.layout = dbc.Container([mygraph, button_group])
+fig1.add_trace(
+        go.Scatter(x=df.timestamp, y=df.whumidity, name="Humidity", line=dict(color='royalblue')),
+    #go.Scatter(x=[2, 3, 4], y=[4, 5, 6], name="yaxis2 data"),
+    secondary_y=True,
+)
+
+# Set y-axes titles
+fig1.update_yaxes(title_text="<b>Temperature</b> (°C)", secondary_y=False)
+fig1.update_yaxes(title_text="<b>Humidity</b> (%)", secondary_y=True)
+fig1.update_layout(
+        uirevision=True,
+        xaxis=dict(
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+    )
+mygraph1.figure = fig1
+#################################
+
+df = get_statistics()
+
+## Page elements
+
+tab = go.Figure()
+tab_graph = dcc.Graph(figure=tab)
+
+slid = dcc.Slider(0, 100, value=10)
+app.layout = dbc.Container([tab_graph, mygraph1, slid])
+## Misc
+graph_range = None
+## Callbacks
 
 @app.callback(
-        Output(mygraph, component_property='figure'),
-
-        Input(radio, component_property='value')
+        Output(tab_graph, component_property='figure'),
+        Input(mygraph1, component_property='relayoutData')
 )
-def update_title (user_input):
-    fig = px.line(data_frame=df, x="timestamp", y=user_input)
-    return fig
+def update_table(rlData):
+    #global graph_range
+    #ffig1 = fig1.full_figure_for_development() 
+    #print(ffig1.layout.xaxis.range)
+    #print(rlData['xaxis.range'][1])
+    graph_range = []
+    try:
+        graph_range = rlData['xaxis.range']
+    except (KeyError, TypeError):
+        graph_range = []
+
+    df = get_statistics(*graph_range)
+
+    tab = go.Figure(data=[go.Table(
+        columnwidth=[80, 80],
+        header=dict(values=['', '<b>Temperature</b>', '<b>Humidity</b>'],
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[['<b>Average</b>', '<b>Maximum</b>', '<b>Mimimum</b>', ],
+                        [df.avg_temperature, df.max_temperature, df.min_temperature],
+                        [df.avg_humidity, df.max_humidity,  df.min_humidity]],
+                   fill_color='lavender',
+                   align='left'))
+    ])
+    #tab = create_stats_page(graph_range)
+    return tab
+
+
+@app.callback(
+        Output(mygraph1, component_property='figure'),
+        Input(slid, component_property='value')
+)
+def update_smoothing (val):
+    df = get_data(val)
+    fig1.update_traces(x=df.timestamp, y=df.whumidity, selector=dict(name="Humidity"))
+    fig1.update_traces(x=df.timestamp, y=df.wtemperature, selector=dict(name="Temperature"))
+    return fig1
 
 
 
